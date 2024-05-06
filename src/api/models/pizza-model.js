9ecef6ab8db2955865485347ffe3cc58810e88fa
Tatu <tatu.sailynoja@gmail.com>
2024-05-06 15:55:52 +0300

    Controller needs to pass user token to model to check for authorization

--- src/api/models/pizza-model.js
+++ src/api/models/pizza-model.js
@@ -2,6 +2,12 @@
 // How to handle errors in controller?
 import promisePool from '../../utils/database.js';
 
+
+/**
+ * List all default pizzas
+ * PLACEHOLDER DO NOT USE
+ * @returns {Promise} Promise object represents the list of all default pizzas
+*/
 const listAllDefaultPizzas = async () => {
    const [rows] = await promisePool.query('SELECT * FROM pizza where user_id = 1');
    rows.forEach( async (row) => {
@@ -13,11 +19,19 @@ const listAllDefaultPizzas = async () => {
    return rows;
 };
 
+/**
+ * List all pizzas from the entire DB
+*/
 const listAllPizzas = async () => {
    const [rows] = await promisePool.query('SELECT * FROM pizza');
    return rows;
 };
 
+/**
+ * Find pizza by id
+ * @param {*} id
+ * @returns {Promise} Promise object represents the pizza object
+*/
 const findPizzaById = async (id) => {
     const [rows] = await promisePool.execute('SELECT * FROM pizza WHERE id = ?', [id]);
     console.log('rows', rows);
@@ -29,29 +43,35 @@ const findPizzaById = async (id) => {
 
 /**
  * Add pizza object and file to database
+ * PLACEHOLDER DO NOT USE
  * @param {*} pizza 
- * @param {*} file 
- * @returns 
+ * @param {*} user_id
+ * @returns FALSE or "Pizza added"
  */
 
-const addPizza = async (pizza, file) => {
-  const {name, price, owner,birthdate} = cat;
-  const filename = file.filename;
-  const sql = `INSERT INTO cats (cat_name, weight, owner, filename, birthdate)
+const addPizza = async (pizza, user_id) => {
+const {/*PIZZA ATTRIBUTES HERE*/} = pizza;
+  const sql = `INSERT INTO pizza (/*ROW NAMES HERE*/)
                VALUES (?, ?, ?, ?, ?)`;
                
-  const params = [cat_name, weight, owner, filename, birthdate];
+  const params = [/* FROM LINE 52 */];
   console.log('sql', params);
     const rows = await promisePool.execute(sql, params);
     console.log('rows', rows);
      if (rows[0].affectedRows === 0) {
         return false;
      }
-    return {cat_id: rows[0].insertId};
+     return ("Pizza added");
 };
 
-const modifyPizza = async (cat, id) => {
-   const sql = promisePool.format(`UPDATE cats SET ? WHERE cat_id = ?`, [cat, id]);
+/**
+ * Modify pizza object in database
+ * @param {*} pizza pizza object to put in database
+ * @param {*} id id of the pizza to modify
+ * @returns FALSE or "success"
+*/
+const modifyPizza = async (pizza, id) => {
+   const sql = promisePool.format(`UPDATE pizza SET ? WHERE pizza_id = ?`, [pizza, id]);
    const rows = await promisePool.execute(sql);
    console.log('rows', rows);
    if (rows[0].affectedRows === 0) {
@@ -60,6 +80,11 @@ const modifyPizza = async (cat, id) => {
    return {message: 'success'};
 };
 
+/**
+ * Remove pizza from database
+ * @param {*} id id of the pizza to remove
+ * @returns FALSE or "success"
+*/
 const removePizza = async (id) => {
    const [rows] = await promisePool.execute('DELETE FROM cats WHERE cat_id = ?', [id]);
    console.log('rows', rows);
--- src/api/routes/pizza-router.js
+++ src/api/routes/pizza-router.js
@@ -2,40 +2,21 @@ import express from 'express';
 import {
   getAllPizzas,
   getPizzaById,
-  postPizza,  
   putPizza,
   deletePizza
 } from '../controllers/pizza-controller.js';
-import multer from 'multer';
 
 const pizzaRouter = express.Router();
 
-const myStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E5);
-    const uniquePrefix = (req.body.name).replaceAll(/,|-|_|'/gi, '').toLowerCase();
-    let extension = file.originalname.split(".").pop(); // Or mimetype?
-    if (extension !== ('jpg' || 'jpeg' || 'png' || 'gif')) {
-      extension = 'jpg';
-    }
-
-    const filename = `${uniquePrefix}-${uniqueSuffix}.${extension}`;
-    cb(null, filename);
-  },
-});
-
-const upload = multer({dest: 'uploads/', myStorage});
-
 pizzaRouter.route('/')
   .get(getAllPizzas)
-  .post(upload.single("filename"), postPizza);
 
 pizzaRouter.route('/:id')
   .get(getPizzaById)
   .put(putPizza)
   .delete(deletePizza);
+
+pizzaRouter.route('/user/:id')
+  .get(getUserPizzas);
 
 export default pizzaRouter;
