1f819f2b6bd74bac38a4cdfc6c4e2acf14bb38b3
Tatu <tatu.sailynoja@gmail.com>
2024-05-09 17:33:58 +0300

    remove expreess

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import api from './api/index.js';
 
-
-const express = require('express');
 const cors = require('cors');
 const app = express();
 
