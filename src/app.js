b6097d23072a1f73761038202d6f9c81b577af87
Tatu <tatu.sailynoja@gmail.com>
2024-05-09 17:10:08 +0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import api from './api/index.js';
 
+
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import app from './app.js';
-const cors = require('cors');
 
 const hostname = 'localhost';
 const port = 3000;
-
-app.use(cors());
-
 
 app.listen(port, hostname, () => {
    console.log(`Server running at http://${hostname}:${port}/`);
