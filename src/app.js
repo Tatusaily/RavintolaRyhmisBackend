ed0736e21ea06f7ad886ada927ea4c1ed9b8445e
Tatu <tatu.sailynoja@gmail.com>
2024-05-09 18:31:20 +0300

    cors fix yourself please

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import cors from 'cors';
 import api from './api/index.js';
 
 const app = express();
+const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
